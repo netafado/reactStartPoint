2bcab86585f50c229156d9ac07480cdca4d98472
Isaias F. dos Santos <isaias.fran@gmail.com>
2017-06-10 20:31:29 -0300

    react started point

---
+++ app.js
@@ -0,0 +1,15 @@
+// Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './css/style.css';
+
+const App = () => {  
+    return(
+        <h1 className="titulo">React</h1>
+      );
+    
+
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
