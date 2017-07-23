299f15dcfa76684e48e623ca707ede32dac885aa
Isaias F. dos Santos <isaias.fran@gmail.com>
2017-07-23 18:23:25 -0300

    add bootstrap e font fontawesome

---
+++ src/main/app.jsx
@@ -0,0 +1,10 @@
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => (
+	<div className='container'>
+		<h1>Tudo funcionando</h1>
+	</div>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,45 @@
+const webpack = require('webpack');
+const ExtratTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './app.js',
-  
-  output: {
-    // path: 'build',
-    filename: 'bundle.js'
-  },
-  
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['react-hot', 'babel-loader'] 
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader' 
-      }
-    ]
-  }
-};
+    entry: './src/index.jsx',
+    output: {
+    	path: __dirname + '/public',
+    	filename: './app.js'
+    },
+    devServer:{
+    	port: 8080,
+    	contentBase: './public'
+    },
+
+    resolve:{
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins:[
+    	new ExtratTextPlugin('app.css')
+    ],
+    module: {
+    	loaders: 
+    	[
+	    	{
+	    		test: /.jsx?$/,
+	    		loader: 'babel-loader',
+	    		exclude: /node_modules/,
+	    		query:
+	    		{
+	    			presets:['es2015', 'react'],
+	    			plugins: ['transform-object-rest-spread']
+	    		}
+	    	},
+	    	{
+    			test: /\.css?$/,
+    			loader: ExtratTextPlugin.extract("style-loader", "css-loader")
+	    	},
+			{
+					test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+					loader: 'file'
+			}
+
+    	]
+    }
+}
